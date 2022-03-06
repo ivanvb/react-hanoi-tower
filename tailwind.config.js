a73688cbc10b828506815033e7312457b0daae2b
ivanvb <ivanvalette@hotmail.com>
2022-03-05 21:42:24 -0400

    chore: initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    content: ['src/**/**.jsx'],
+    theme: {
+        extend: {
+            container: {
+                center: true,
+                padding: {
+                    DEFAULT: '1rem',
+                    md: '2rem',
+                },
+            },
+        },
+    },
+};
