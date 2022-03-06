fe3a56116b264318abf729907504fc3be14f56e5
ivanvb <ivanvalette@hotmail.com>
2022-03-05 21:46:35 -0400

    feat: add Hanoi Controller

---
+++ src/controller/HanoiController.js
@@ -0,0 +1,66 @@
+const initialData = {
+    blocks: {
+        b1: {
+            color: '#9B287B',
+        },
+        b2: {
+            color: '#5C174E',
+        },
+        b3: {
+            color: '#402039',
+        },
+        b4: {
+            color: '#170F11',
+        },
+    },
+    containers: [
+        {
+            id: 'c1',
+            blocks: ['b1', 'b2', 'b3', 'b4'],
+        },
+        {
+            id: 'c2',
+            blocks: [],
+        },
+        {
+            id: 'c3',
+            blocks: [],
+        },
+    ],
+};
+
+export function getData() {
+    return { ...initialData };
+}
+
+export function getBlockId(block) {
+    return Number?.parseInt(block?.replace('b', '')) || null;
+}
+
+export function canMove(state, destinationId, draggableId) {
+    const destinationTop = state.containers.find((el) => el.id === destinationId)?.blocks?.[0];
+
+    const draggableParsedId = getBlockId(draggableId);
+    const destinationParsedId = getBlockId(destinationTop) || Number.POSITIVE_INFINITY;
+
+    return draggableParsedId < destinationParsedId;
+}
+
+export function performMovement(state, draggableId, sourceId, destinationId) {
+    const { containers } = state;
+    containers.forEach((el) => {
+        const { id, blocks } = el;
+        if (id === destinationId) {
+            blocks.unshift(draggableId);
+        }
+
+        if (id === sourceId) {
+            blocks.shift();
+        }
+    });
+
+    return {
+        ...state,
+        containers,
+    };
+}
