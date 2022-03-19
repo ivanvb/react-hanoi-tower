bc3d09b03d724402da2fb3ca09397053deadf1bd
ivanvb <ivanvalette@hotmail.com>
2022-03-18 22:47:45 -0400

    fix: level selector layout on mobile

--- src/components/Modal/SettingsModal.jsx
+++ src/components/Modal/SettingsModal.jsx
@@ -15,16 +15,21 @@ const SettingsModal = ({ onSettingsClose, onLevelSelect, onDataClear }) => {
                             {levels.map((level, i) => {
                                 return (
                                     <button
-                                        className="bg-[#023a63] text-white w-20 h-20 rounded relative mx-auto"
                                         key={i}
-                                        onClick={() => {
-                                            onLevelSelect(level);
-                                            close();
-                                        }}
+                                        className="relative before:pt-[100%] before:w-full before:block"
                                     >
-                                        <div className="absolute w-full px-4 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
-                                            <span className="block mb-1 text-xl">{level}</span>
-                                            <LevelRating rating={2} className="w-12 pt-2" />
+                                        <div
+                                            className="bg-[#023a63] text-white rounded absolute mx-auto h-full w-full top-0 left-0"
+                                            key={i}
+                                            onClick={() => {
+                                                onLevelSelect(level);
+                                                close();
+                                            }}
+                                        >
+                                            <div className="absolute w-full px-4 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+                                                <span className="block mb-1 text-xl">{level}</span>
+                                                <LevelRating rating={2} className="w-12 pt-2" />
+                                            </div>
                                         </div>
                                     </button>
                                 );
