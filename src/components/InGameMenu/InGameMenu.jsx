a8c17f6cf6314923deae9334875761b5627cefa8
ivanvb <ivanvalette@hotmail.com>
2022-03-23 00:31:31 -0400

    perf: use React memo with InGameMenu component

--- src/components/InGameMenu/InGameMenu.jsx
+++ src/components/InGameMenu/InGameMenu.jsx
@@ -4,44 +4,48 @@ import { MdTouchApp } from 'react-icons/md';
 import { RiDragMoveFill } from 'react-icons/ri';
 import { IoMdSettings } from 'react-icons/io';
 
-const ICON_SIZE = 20;
-const InGameMenu = ({
-    idealMoves,
-    moves,
-    isDragEnabled,
-    onDragToggle,
-    onReset,
-    onSettingsClick,
-}) => {
+function arePropsEqual(prevProps, props) {
     return (
-        <div className="flex items-center justify-between h-12 px-4 py-8 mb-6 tracking-wide text-center border-2 rounded shadow-lg bg-primary-700 border-accent-500">
-            <div>
-                Ideal Moves
-                <br />
-                {idealMoves}
-            </div>
-            <div>
-                Moves
-                <br />
-                {moves}
-            </div>
-            <div className="space-x-4 lg:space-x-5">
-                <button onClick={onDragToggle}>
-                    {isDragEnabled ? (
-                        <MdTouchApp size={ICON_SIZE} />
-                    ) : (
-                        <RiDragMoveFill size={ICON_SIZE} />
-                    )}
-                </button>
-                <button onClick={onReset}>
-                    <GrPowerReset className="reset-icon" size={ICON_SIZE} />
-                </button>
-                <button onClick={onSettingsClick}>
-                    <IoMdSettings size={ICON_SIZE} />
-                </button>
-            </div>
-        </div>
+        prevProps.idealMoves === props.idealMoves &&
+        prevProps.moves === props.moves &&
+        prevProps.isDragEnabled === props.isDragEnabled
     );
-};
+}
+
+const ICON_SIZE = 20;
+const InGameMenu = React.memo(
+    ({ idealMoves, moves, isDragEnabled, onDragToggle, onReset, onSettingsClick }) => {
+        return (
+            <div className="flex items-center justify-between h-12 px-4 py-8 mb-6 tracking-wide text-center border-2 rounded shadow-lg bg-primary-700 border-accent-500">
+                <div>
+                    Ideal Moves
+                    <br />
+                    {idealMoves}
+                </div>
+                <div>
+                    Moves
+                    <br />
+                    {moves}
+                </div>
+                <div className="space-x-4 lg:space-x-5">
+                    <button onClick={onDragToggle}>
+                        {isDragEnabled ? (
+                            <MdTouchApp size={ICON_SIZE} />
+                        ) : (
+                            <RiDragMoveFill size={ICON_SIZE} />
+                        )}
+                    </button>
+                    <button onClick={onReset}>
+                        <GrPowerReset className="reset-icon" size={ICON_SIZE} />
+                    </button>
+                    <button onClick={onSettingsClick}>
+                        <IoMdSettings size={ICON_SIZE} />
+                    </button>
+                </div>
+            </div>
+        );
+    },
+    arePropsEqual
+);
 
 export default InGameMenu;
