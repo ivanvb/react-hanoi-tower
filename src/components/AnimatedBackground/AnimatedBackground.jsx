ca6fd9b6026e16f43aa20f51186fd69cd827cc40
ivanvb <ivanvalette@hotmail.com>
2022-03-24 17:36:00 -0400

    fix: duplicate delay's random interval
    As the animation is alternated, by duplicating the interval the
    initial delay can appear in both ends of the animation timeline.

--- src/components/AnimatedBackground/AnimatedBackground.jsx
+++ src/components/AnimatedBackground/AnimatedBackground.jsx
@@ -4,7 +4,7 @@ import { getRandomInt } from '../../utils';
 const MIN_SPEED = 20;
 const MAX_SPEED = 60;
 
-const circles = [...Array(getRandomInt(8, 25))].map(() => {
+const circles = [...Array(getRandomInt(12, 25))].map(() => {
     const speedX = getRandomInt(MIN_SPEED, MAX_SPEED);
     const speedY = getRandomInt(MIN_SPEED, MAX_SPEED);
 
@@ -15,8 +15,8 @@ const circles = [...Array(getRandomInt(8, 25))].map(() => {
             y: speedY,
         },
         delay: {
-            x: getRandomInt(speedX * -1, 0),
-            y: getRandomInt(speedY * -1, 0),
+            x: getRandomInt(speedX * -2, 0),
+            y: getRandomInt(speedY * -2, 0),
         },
     };
 });
