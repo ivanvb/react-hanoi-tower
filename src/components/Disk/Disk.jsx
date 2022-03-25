84534e389c0c46b33ed8b66ff42f3d4a8e4e29a8
ivanvb <ivanvalette@hotmail.com>
2022-03-24 23:24:27 -0400

    feat: add responsive disks height

--- src/components/Disk/Disk.jsx
+++ src/components/Disk/Disk.jsx
@@ -11,13 +11,13 @@ const Disk = forwardRef(({ className, color, diskId, style, innerRef, ...rest },
     return (
         <div ref={setRef} {...rest} style={style} className={`${className} disk`}>
             <div
-                className="relative h-[14px] lg:h-[16.5px] rounded-xl disk-content"
+                className="relative h-[12px] tall:h-[14px] lg:h-[16.5px] rounded-xl disk-content"
                 style={{
                     backgroundColor: color,
                     '--disk-id': diskId,
                 }}
             >
-                <span className="absolute block text-xs -translate-x-1/2 -translate-y-1/2 select-none top-1/2 left-1/2">
+                <span className="absolute block text-[0.625rem] tall:text-xs -translate-x-1/2 -translate-y-1/2 select-none top-1/2 left-1/2">
                     {diskId}
                 </span>
             </div>
